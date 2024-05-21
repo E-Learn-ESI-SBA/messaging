6e5b6051230eb974d644bf7fd51b71d0936246ce
FendiMohamed <m.fendi@esi-sba.dz>
2024-05-21 01:36:01 +0100

    add create chat if sending first message

--- controllers/chat/one-to-one-chat.ts
+++ controllers/chat/one-to-one-chat.ts
@@ -3,47 +3,34 @@ import  ChatModel  from "../../models/chat";
 import UserModel from "../../models/user";
 export const oneToOneChat = async function (req: any, res : FastifyReply) {
     const { userId } = req.body;
-
-    if (!userId) {
-      console.log("UserId param not sent with request");
-      return res.status(400);
+   try {
+   
+        let isChat = await ChatModel.find({
+          isGroup: false,
+          $and: [
+            { users: { $elemMatch: { $eq: "664bc4f10e0d0506d19c60c7"} } },
+            // { users: { $elemMatch: { $eq: req.user._id } } },
+            { users: { $elemMatch: { $eq: userId } } },
+          ],
+        })
+          .populate("users")
+          .populate("latestMessage");
+        //@ts-ignore
+        isChat = await UserModel.populate(isChat, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        
+        if (isChat.length > 0) {
+          res.send( {chat :  isChat[0]});
+      }else {
+        res.send({chat : [] , message : 'there is no chat' });
     }
-    
-    var isChat = await ChatModel.find({
-      isGroup: false,
-      $and: [
-        { users: { $elemMatch: { $eq: "664bc4f10e0d0506d19c60c7"} } },
-        // { users: { $elemMatch: { $eq: req.user._id } } },
-        { users: { $elemMatch: { $eq: userId } } },
-      ],
-    })
-      .populate("users", "-password")
-      .populate("latestMessage");
-    //@ts-ignore
-    isChat = await UserModel.populate(isChat, {
-      path: "latestMessage.sender",
-      select: "name pic email",
-    });
-    
-    if (isChat.length > 0) {
-      res.send(isChat[0]);
-    } else {
-      var chatData = {
-        isGroup: false,
-        users: ["664bc4f10e0d0506d19c60c7", userId],
-      };
-    
-      try {
-        const createdChat = await ChatModel.create(chatData);
-        const FullChat = await ChatModel.findOne({ _id: createdChat._id }).populate(
-          "users",
-        );
-        res.status(201).send(FullChat);
-      } catch (error) {
+}
+       catch (error) {
         res.status(400);
       }
-    }
-};
+    };
 
 
 
--- controllers/message/send-message.ts
+++ controllers/message/send-message.ts
@@ -1,19 +1,38 @@
 import ChatModel from "../../models/chat";
 import MessageModel  from "../../models/message";
 export const sendMessage = async function (req: any, res : any) {
-  const { text, chatId  }  = req.body;
+  const { text, chatId ,userId }  = req.body;
+  const userTokenId = "663aba6c8cdc7aa61f324b1a"
   console.log('message',req.body)
   try {
-   let message = await MessageModel.create({
-    sender: "663aba6c8cdc7aa61f324b1a" ,
-    // sender: req.user._id,
-    text: text,
-    chat: chatId,
-  });
-
- await ChatModel.findByIdAndUpdate(req.body.chatId, { latestMessage: message._id });
-
-    res.status(201).send('message created successfully');
+    if(chatId){
+      let message = await MessageModel.create({
+        sender: userTokenId ,
+        // sender: req.user._id,
+        text: text,
+        chat: chatId,
+      });
+      await ChatModel.findByIdAndUpdate(req.body.chatId, { latestMessage: message._id });
+      res.status(201).send('message created successfully');
+    }else{
+      let chatData = {
+        isGroup: false,
+        users: [userId,userTokenId],
+        latestMessage : null,
+        // users: [req.user._id, userId],
+      };
+        const createdChat = await ChatModel.create(chatData);
+        console.log('tt',createdChat)
+        let message = await MessageModel.create({
+          sender: userTokenId ,
+          // sender: req.user._id,
+          text: text,
+          chat: createdChat._id ,
+        });
+        const FullChat = await ChatModel.findByIdAndUpdate(createdChat._id,{latestMessage : message._id }, 
+          { new: true }).populate("users")
+        res.status(201).send(FullChat);
+    }
   } catch (error) {
    res.status(400);
     return error
